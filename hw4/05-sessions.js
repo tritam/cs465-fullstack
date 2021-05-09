a3e186c3749cc300ad8972e9c3cf7836780dfff1
Tristan Tam <tristantam@gmail.com>
2021-05-09 00:19:43 -0700

    Progress on HW4... I'm finished I'm tired lol

--- hw4/05-sessions.js
+++ hw4/05-sessions.js
@@ -6,10 +6,27 @@ const port = process.env.PORT || 5000;
 // Add your code here
 
 // Use the express-session module
-app.use(/** ... */);
+app.use(session({
+  'store':new session.MemoryStore(),
+  'secret': "Shh, its a secret!",
+  'resave':false,
+  'saveUninitialized':false,
+  'cookie':{
+    'maxAge':86400000
+  }
+}));
 
 app.get('/', (req, res) => {
   // Add your code here
+  if(req.session.page_views) {
+    req.session.page_views++;
+    res.send("Currently on route: "+req.path+`<br><br>`+"Previously visit: "+`<br>`);
+  } 
+  else {
+    req.session.page_views = 1;
+    let fullUrl = req.protocol+'://'+req.get('host')+req.originalUrl;
+    res.send("Currently on route: "+req.path+`<br><br>`+"Welcome to "+fullUrl);
+  }
 });
 
 app.listen(port, () => {
