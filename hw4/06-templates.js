3396443216ed5fd1cec24f088413de90a6591617
Tristan Tam <tristantam@gmail.com>
2021-05-14 00:07:05 -0700

    Finished HW4 06-templates

--- hw4/06-templates.js
+++ hw4/06-templates.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,6 +11,35 @@ app.set('view engine', 'pug');
 const url = 'https://restcountries.eu/rest/v2/all';
 
 // Add your code here
+var capitals = [];
+var populous = [];
+var regions = []; 
+
+function countOccurence(arr, continent) {
+  var count = 0; 
+
+  arr.sort();
+  for (var i = 0; i < arr.length; i++) {
+    if(arr[i] === continent) {
+      count = count + 1;
+    }
+  }  
+
+  return count;
+}
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    for (var i=0; i < data.length; i++) { 
+      capitals.push(data[i].name + ' - ' + data[i].capital);
+      regions.push(data[i].region);
+      if(data[i].population >= 50000000) {
+        populous.push(data[i].name + ' - ' + data[i].population.toLocaleString());
+      }
+    }
+  })
+  .catch((error) => console.log(error));
 
 app.get('/', (req, res) => {
   // render pug template for the index.html file
@@ -30,7 +60,7 @@ app.get('/capitals', (req, res) => {
   // map the output array to create an array with country names and capitals
   // check for empty data in the output array
 
-  results = ['Afghanistan', 'Aland Islands', 'Albania'];
+  results = capitals;
 
   res.render('page', {
     heading: 'Countries and Capitals',
@@ -43,7 +73,7 @@ app.get('/populous', (req, res) => {
   // sort the resulting array to show the results in order of population
   // map the resulting array into a new array with the country name and formatted population
 
-  results = ['China', 'India', 'United States of America'];
+  results = populous;
 
   res.render('page', {
     heading: 'Most Populous Countries',
@@ -55,7 +85,14 @@ app.get('/regions', (req, res) => {
   // reduce the output array in a resulting object that will feature the numbers of countries in each region
   // disregard empty data from the output array
 
-  results = ['Asia - 50', 'Europe - 53', 'Africa - 60'];
+
+  results = ['Asia - ' + countOccurence(regions, 'Asia'), 
+    'Europe - ' + countOccurence(regions, 'Europe'), 
+    'Africa - ' + countOccurence(regions, 'Africa'), 
+    'Oceania - ' + countOccurence(regions, 'Oceania'), 
+    'Americas - ' + countOccurence(regions, 'Americas'),
+    'Polar - ' + countOccurence(regions, 'Polar')
+  ];
 
   res.render('page', {
     heading: 'Regions of the World',
